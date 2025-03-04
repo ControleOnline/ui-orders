c147b2d56e45f60855df46e7899dcfdd3ad14691
Luiz Kim <luiz.kim@controleonline.com>
2025-03-03 21:34:21 -0300

    Update

--- src/components/cart/addProduct.vue
+++ src/components/cart/addProduct.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="row col-12 product-add ">
-    <q-btn class="full-width q-pa-xs btn-primary" label="Comprar" />
+  <div class="row col-12 product-add">
+    <div class="col flex items-center justify-center">
+      <ProductQuantity
+        :product="product"
+        @increaseQuantity="increaseQuantity"
+        @decreaseQuantity="decreaseQuantity"
+      />
+    </div>
+    <div class="col flex items-center justify-center">
+      <q-btn
+        class="full-width q-pa-xs btn-primary"
+        label="Adicionar ao Carrinho"
+      />
+    </div>
   </div>
 </template>
+<script>
+import ProductQuantity from "@controleonline/ui-orders/src/components/ProductQuantity.vue";
+export default {
+  components: {
+    ProductQuantity,
+  },
+  props: {
+    product: {
+      required: true,
+    },
+  },
+};
+</script>
--- src/components/cart/bottomCart.vue
+++ src/components/cart/bottomCart.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row col-12 bottom-cart sticky-bottom bg-white q-pa-md">
-    <div class="col-8">R$ 20,00</div>
-    <div class="col-4">
+    <div class="col flex items-center justify-center">R$ 20,00</div>
+    <div class="col flex items-center justify-center">
       <q-btn class="full-width q-pa-xs btn-primary" label="Ver Carrinho" />
     </div>
   </div>
