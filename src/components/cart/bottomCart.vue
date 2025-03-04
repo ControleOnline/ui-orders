4d0feccbdc903e29b8b25af8f33523276ec6238d
Luiz Kim <luiz.kim@controleonline.com>
2025-03-03 21:52:18 -0300

    Update

--- src/components/cart/bottomCart.vue
+++ src/components/cart/bottomCart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row col-12 bottom-cart sticky-bottom bg-white q-pa-md">
+  <div class="row col-12 bottom-cart sticky-bottom full-width bg-white q-pa-md">
     <div class="col flex items-center justify-center">R$ 20,00</div>
     <div class="col flex items-center justify-center">
       <q-btn class="full-width q-pa-xs btn-primary" label="Ver Carrinho" />
