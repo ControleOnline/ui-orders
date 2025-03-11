c743e1387c6d5f93332c51773842c4ed89dbc975
Luiz Kim <luiz.kim@controleonline.com>
2025-03-11 15:18:32 -0300

    Update

--- src/store/orders/react.js
+++ src/store/orders/react.js
@@ -2,8 +2,15 @@ import {useState} from 'react';
 import storeModule from './index';
 
 export const ordersStore = () => {
-  const [state] = useState(storeModule.state);
-  const commit = (type, payload) => {};
+  const [state, setState] = useState(storeModule.state);
+  const commit = (type, payload) => {
+    const mutation = storeModule.mutations[type](state, payload);
+    if (mutation) {
+      const newState = {...state};
+      mutation(newState, payload);
+      setState(newState);
+    }
+  };
   return {
     getters: state,
     actions: storeModule.actions,
