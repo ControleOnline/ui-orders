c4706cc589e4179ea01893e2b2c8e7f41ac6c77e
Luiz Kim <luiz.kim@controleonline.com>
2025-03-21 17:49:16 -0300

    Update

--- src/react/pages/orders/sales/index.js
+++ src/react/pages/orders/sales/index.js
@@ -48,7 +48,7 @@ const Orders = ({navigation}) => {
         config &&
         Object.entries(config).length > 0 &&
         device &&
-        config['config-version' == device.buildNumber]
+        config['config-version'] == device.buildNumber
       )
         setPdvType(config['pdv-type'] || 'full');
       else if (
