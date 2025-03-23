08c90df80b6d62c6857cded182ed3c255e7dddc5
Luiz Kim <luiz.kim@controleonline.com>
2025-03-22 23:41:32 -0300

    Update

--- src/react/services/Cielo/Checkout.js
+++ src/react/services/Cielo/Checkout.js
@@ -157,7 +157,7 @@ export default Checkout = ({route}) => {
   const handleConfirmValue = () => {
     const valorNumerico = parseFloat(inputValue.replace(/\D/g, '')) / 100;
     if (isNaN(valorNumerico) || valorNumerico <= 0) {
-      showErrorPopup('Por favor, insira um valor válido!');
+      paymentTypeActions.setError('Por favor, insira um valor válido!');
       return;
     }
 
