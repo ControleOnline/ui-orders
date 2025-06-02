48098e3f7e5792e8bf7814fce5b6b554f8a376da
Luiz Kim <luiz.kim@controleonline.com>
2025-06-02 18:07:03 -0300

    Log to buffers

--- src/react/components/OrderTotalToolbar.js
+++ src/react/components/OrderTotalToolbar.js
@@ -21,50 +21,73 @@ export default OrderTotalToolbar = ({route}) => {
   const [productBuffer, setProductBuffer] = useState([]);
   const timeoutId = useRef(null);
 
-  const persistProducts = useCallback(() => {
-    console.log(currentRoute?.name, productBuffer.length);
-    if (
-      order &&
-      order['@id'] &&
-      productBuffer.length > 0 &&
-      currentRoute?.name &&
-      currentRoute?.name != 'ProductsPage'
-    ) {
-      if (timeoutId.current) clearTimeout(timeoutId.current);
-      const currentOrder = {...order};
-      const currentProducts = [...productBuffer];
-
-      timeoutId.current = setTimeout(() => {
-        console.log(currentProducts);
-        addProducts(currentOrder, currentProducts);
-        setProductBuffer([]); //Aqui deveria zerar (E zera, mas depois volta)
-        timeoutId.current = null;
-      }, 100);
-    }
-  }, [currentRoute, productBuffer, order, setProductBuffer]);
-
-  const addProducts = useCallback(
-    (currentOrder, currentProducts) => {
-      if (currentProducts.length > 0 && currentOrder && currentOrder['@id'])
-        ordersActions.addProducts(
-          currentOrder['@id'].replace(/\D/g, ''),
-          currentProducts,
-        );
-    },
-    [productBuffer, setProductBuffer],
+  useFocusEffect(
+    useCallback(() => {
+      console.log(currentRoute?.name, productBuffer.length);
+      if (
+        order &&
+        order['@id'] &&
+        productBuffer.length > 0 &&
+        currentRoute?.name &&
+        currentRoute?.name != 'ProductsPage'
+      )
+        persistProducts();
+    }, [currentRoute, productBuffer, order]),
   );
 
-  useEffect(() => {
-    const handleAddProduct = data => {
-      console.log('Adicionando no Buffer (Não adiciona novamente)');
-      setProductBuffer(prev => [...prev, data]);
-      setTimeout(() => {
-        persistProducts();
-      }, 300);
-    };
-    eventBus.on('add-product', handleAddProduct);
-    return () => eventBus.off('add-product', handleAddProduct);
-  }, [setProductBuffer]);
+  const persistProducts = () => {
+    if (timeoutId.current) clearTimeout(timeoutId.current);
+
+    timeoutId.current = setTimeout(() => {
+      const currentOrder = {...order};
+      const currentProducts = [...productBuffer];
+      console.log(currentProducts);
+      addProducts(currentOrder, currentProducts);
+      timeoutId.current = null;
+    }, 200);
+  };
+
+  const addProducts = useCallback((currentOrder, currentProducts) => {
+    if (currentProducts.length > 0 && currentOrder && currentOrder['@id'])
+      ordersActions.addProducts(
+        currentOrder['@id'].replace(/\D/g, ''),
+        currentProducts,
+      );
+    setProductBuffer([]);
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      const handleAddProduct = data => {
+        console.log('Adicionando ao buffer:', {
+          atual: productBuffer,
+          novo: data,
+        });
+
+        try {
+          console.log(typeof setProductBuffer);
+          setProductBuffer(prev => {
+            console.log(prev);
+            const updatedBuffer = [...prev, data];
+            console.log('Buffer atualizado dentro do set:', updatedBuffer);
+            return updatedBuffer;
+          });
+        } catch (error) {
+          console.log('Error', error);
+        }
+        setTimeout(() => {
+          console.log(
+            'Estado após 100ms:',
+            productBuffer,
+            'Dados adicionados:',
+            data,
+          );
+        }, 100);
+      };
+      eventBus.on('add-product', handleAddProduct);
+      return () => eventBus.off('add-product', handleAddProduct);
+    }, []),
+  );
 
   useFocusEffect(
     useCallback(() => {
@@ -82,7 +105,7 @@ export default OrderTotalToolbar = ({route}) => {
 
       eventBus.on('price', listener);
       return () => eventBus.off('price', listener);
-    }, [price, setPrice]),
+    }, [price]),
   );
 
   return !order ? (
