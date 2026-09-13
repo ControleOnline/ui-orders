835d386f9eb5d80678f35cec84a5168475455aa2
Luiz Kim <luizkim@users.noreply.github.com>
2026-09-13 20:55:13 -0300

    test(#797): keep staging endpoint isolated

--- src/tests/browser/apiOrigin.js
+++ src/tests/browser/apiOrigin.js
@@ -1,13 +1,9 @@
 const { APP_ENV } = require('../../../config/env');
 
 const API_ORIGIN = String(APP_ENV?.API_PLAYWRIGHT || '').replace(/\/$/, '');
-const API_ORIGINS = [...new Set([
-  API_ORIGIN,
-  String(APP_ENV?.API_ENTRYPOINT || '').replace(/\/$/, ''),
-].filter(Boolean))];
 
 if (!API_ORIGIN) {
   throw new Error('API_PLAYWRIGHT nao configurado.');
 }
 
-module.exports = { API_ORIGIN, API_ORIGINS };
+module.exports = { API_ORIGIN };
